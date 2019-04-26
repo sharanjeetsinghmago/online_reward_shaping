ca31572ef612ed949bd7398c275f56066d716280
m-luebbers <matthew.luebbers@colorado.edu>
2019-04-26 13:44:03 -0600

    Probably final reward representation code
    Made desired path relatively high reward (though not nearly as high as high-reward rocks), have 7 filters for different types of terrain and targets

--- src/image2reward.py
+++ src/image2reward.py
@@ -17,6 +17,11 @@ def color_detect(img): # assign reward value: -1
     # Hue [0, 179], Saturation [0, 255], Value [0, 255]
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
+    #stars
+    hsv_min_star = np.array([29,255,255])
+    hsv_max_star = np.array([29,255,255])
+    mask_star = cv2.inRange(hsv, hsv_min_star, hsv_max_star)
+
     # red region 1
     hsv_min_red = np.array([0, 127, 0])
     hsv_max_red = np.array([30, 255, 255])
@@ -27,9 +32,13 @@ def color_detect(img): # assign reward value: -1
     mask2_red = cv2.inRange(hsv, hsv_min_red, hsv_max_red)
 
     # light gray region
-    hsv_min_lgray = np.array([0, 0, 100])
-    hsv_max_lgray = np.array([40, 60, 255])
-    mask_lgray = cv2.inRange(hsv, hsv_min_lgray, hsv_max_lgray)
+    hsv_min_lgray = np.array([0, 0, 160])
+    hsv_max_lgray = np.array([100, 20, 255])
+    mask1_lgray = cv2.inRange(hsv, hsv_min_lgray, hsv_max_lgray)
+
+    hsv_min_lgray = np.array([10, 0, 120])
+    hsv_max_lgray = np.array([20, 50, 180])
+    mask2_lgray = cv2.inRange(hsv, hsv_min_lgray, hsv_max_lgray)
 
     # dark gray region
     hsv_min_dgray = np.array([85, 0, 0])
@@ -60,7 +69,7 @@ def color_detect(img): # assign reward value: -1
     hsv_max_green = np.array([140, 255, 200])
     mask_green = cv2.inRange(hsv, hsv_min_green, hsv_max_green)
 
-    return mask1_red+mask2_red, mask_lgray, mask_dgray, mask1_spink+mask2_spink, mask_white, mask_yellow, mask_green
+    return mask_star, mask1_red+mask2_red, mask1_lgray+mask2_lgray, mask_dgray, mask1_spink+mask2_spink, mask_white, mask_yellow, mask_green
 
 
 
@@ -72,10 +81,11 @@ def image2reward(image_file):
     rewardMatrix = np.zeros((img.shape[0], img.shape[1]))
 
     # extract specific color pixels (each mask * has 966*1048 pixels)
-    mask_red, mask_lgray, mask_dgray, mask_spink, mask_white, mask_yellow, mask_green = color_detect(img)
+    mask_star, mask_red, mask_lgray, mask_dgray, mask_spink, mask_white, mask_yellow, mask_green = color_detect(img)
     #print(mask_red.shape, mask_lgray.shape, mask_dgray.shape, mask_spink.shape)
 
     # from each mask *, extract the color regions
+    index_star = np.vstack((np.where(mask_star == 255)[0], np.where(mask_star == 255)[1]))
     index_red = np.vstack((np.where(mask_red == 255)[0], np.where(mask_red == 255)[1]))
     index_lgray = np.vstack((np.where(mask_lgray == 255)[0], np.where(mask_lgray == 255)[1]))
     index_dgray = np.vstack((np.where(mask_dgray == 255)[0], np.where(mask_dgray == 255)[1]))
@@ -85,26 +95,29 @@ def image2reward(image_file):
     index_green = np.vstack((np.where(mask_green == 255)[0], np.where(mask_green == 255)[1]))
 
     # add reward points to the initial reward heatmap (np.zeros((img.shape[0], img.shape[1])))
+    for i in range(index_star.shape[1]):
+        rewardMatrix[index_star[0][i]][index_star[1][i]] = 50
+
     for i in range(index_yellow.shape[1]):
-        rewardMatrix[index_yellow[0][i]][index_yellow[1][i]] = -4
+        rewardMatrix[index_yellow[0][i]][index_yellow[1][i]] = 1
 
     for i in range(index_red.shape[1]):
-        rewardMatrix[index_red[0][i]][index_red[1][i]] = -3
+        rewardMatrix[index_red[0][i]][index_red[1][i]] = -5
 
     for i in range(index_dgray.shape[1]):
-        rewardMatrix[index_dgray[0][i]][index_dgray[1][i]] = -2
+        rewardMatrix[index_dgray[0][i]][index_dgray[1][i]] = -3
 
     for i in range(index_spink.shape[1]):
-        rewardMatrix[index_spink[0][i]][index_spink[1][i]] = -1
+        rewardMatrix[index_spink[0][i]][index_spink[1][i]] = -2
 
     for i in range(index_lgray.shape[1]):
-        rewardMatrix[index_lgray[0][i]][index_lgray[1][i]] = 0
+        rewardMatrix[index_lgray[0][i]][index_lgray[1][i]] = 10
 
     for i in range(index_white.shape[1]):
-        rewardMatrix[index_white[0][i]][index_white[1][i]] = 1
+        rewardMatrix[index_white[0][i]][index_white[1][i]] = 2
 
     for i in range(index_green.shape[1]):
-        rewardMatrix[index_green[0][i]][index_green[1][i]] = 2
+        rewardMatrix[index_green[0][i]][index_green[1][i]] = 5
 
     # from "pixel coordinate" to "xy coordinate"
     rewardMatrix = rewardMatrix.T
@@ -131,13 +144,13 @@ def image2reward(image_file):
     ax1.set_aspect('equal')
     ax1.set_xlabel('x [m]'); ax1.set_ylabel('y [m]')
     ax1.set_title('Initial Heat Map in Atacama Desert')
-    im1 = ax1.pcolor(xgrid_show, ygrid_show, rewardMatrix, cmap='jet', vmin=-4.0, vmax=2.0)
+    im1 = ax1.pcolor(xgrid_show, ygrid_show, rewardMatrix, cmap='jet', vmin=-5.0, vmax=10.0)
     ax1_divider = make_axes_locatable(ax1)
     cax1 = ax1_divider.append_axes("right", size="7%", pad="2%")
     fig.colorbar(im1, cax=cax1)
     ax1.plot(start[0], start[1], c='lime', marker='x')
     ax1.plot(goal[0], goal[1], c='gold', marker='o')
-    plt.savefig('../img/image2reward_with_rrt_tmp.jpg')
+    plt.savefig('../img/image2reward_with_rrt_tmp.png')
 
 def main():
 
