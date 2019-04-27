c66ba32e690f6682c5c1056e69949d267c769e0a
shoheiw <shoheiwakayama@gmail.com>
2019-04-26 19:20:45 -0600

    Add files via upload

--- src/a_star_with_costmap.py
+++ src/a_star_with_costmap.py
@@ -6,7 +6,7 @@ Created on Thu Mar 21 00:01:09 2019
 @author: shohei
 """
 from functools import wraps
-import time
+import time 
 
 import numpy as np
 import math
@@ -28,11 +28,11 @@ def stopWatch(func):
 """
 image2reward
 """
-def color_detect(img):
+def color_detect(img): 
     # transform to HSV color space
     # Hue [0, 179], Saturation [0, 255], Value [0, 255]
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
+    
     #stars
     hsv_min_star = np.array([29,255,255])
     hsv_max_star = np.array([29,255,255])
@@ -92,26 +92,26 @@ def color_detect(img):
 a star algorithm
 """
 class Node:
-
+    
     def __init__(self, x, y, cost, pind):
         self.x = x
         self.y = y
         self.cost = cost
         self.pind = pind # parent index
-
+        
 
 def calc_obstacle_map(rewardMatrix):
     obmap = (rewardMatrix == -5)
     # print(obmap.shape) (1048, 966) --> (1001, 1001)
     # print(obmap) ([[False, False, ..., False]...[False, ... , False]])
-
+    
     return obmap
 
 
 def get_motion_model():
     # motion primitive (column 0 & 1) + moving cost (column 2)
     motion = [[1, 0, 1],                # right
-              [0, 1, 1],                # top
+              [0, 1, 1],                # top 
               [-1, 0, 1],               # left
               [0, -1, 1],               # bottom
               [-1, -1, math.sqrt(2)],   # bottom left
@@ -119,13 +119,13 @@ def get_motion_model():
               [1, -1, math.sqrt(2)],    # bottom right
               [1, 1, math.sqrt(2)],     # top rihgt
               ]
-
-    return motion
+    
+    return motion    
 
 
 # this function returns the 'key' of each openset component
 def calc_index(node, xwidth, minx, miny):
-
+    
     return (xwidth * node.y) + node.x
 
 
@@ -133,7 +133,7 @@ def calc_index(node, xwidth, minx, miny):
 def calc_heuristic(n1, n2):
     # w is the weight of heuristic. So, this is the tuning parameter of this algorithm.
     w = 1
-    d = w * math.sqrt((n1.x - n2.x)**2 + (n1.y - n2.y)**2)
+    d = w * math.sqrt((n1.x - n2.x)**2 + (n1.y - n2.y)**2)    
     return d
 
 
@@ -146,10 +146,10 @@ def verify_node(node, obmap, minx, miny, maxx, maxy):
         return False
     elif node.y >= maxy:
         return False
-
+    
     if obmap[node.x][node.y]:
         return False
-
+    
     return True
 
 
@@ -162,7 +162,7 @@ def calc_final_path(ngoal, closedset, greso):
         rx.append(n.x * greso)
         ry.append(n.y * greso)
         pind = n.pind
-
+    
     return rx, ry
 
 
@@ -182,34 +182,34 @@ def a_star_planning(sx, sy, gx, gy, rewardMatrix, greso, rs, xwidth, minx, miny,
     maxx:   maximum x coordinate [m]
     maxy:   maximum y coordinate [m]
     """
-
+    
     """
     preparation
     """
     # cost map
     costMatrix = ((rewardMatrix * -1) + 10).flatten()
+    
+    costMatrix[501000] = -1000    
 
-    costMatrix[501000] = -1000
-
-
+    
     # initialize the start & goal nodes
     nstart = Node(math.floor(sx/greso), math.floor(sy/greso), 0.0, -1)
     ngoal = Node(math.floor(gx/greso), math.floor(gy/greso), 0.0, -1)
-
+    
     # create an obstacle map (in this np.array, if obstacle -> True, else -> False)
     obmap = calc_obstacle_map(rewardMatrix)
-
+    
     # get_motion_model returns the motion-with-cost model in this simulation
     motion = get_motion_model()
-
+    
     # make empty dictionary for openset and closedset
-    openset, closedset = dict(), dict()
-
-    # openset is a gigantic 1D dictionary.
+    openset, closedset = dict(), dict()    
+    
+    # openset is a gigantic 1D dictionary. 
     # in this dict, calc_index plays part in "key" connecting the node info (= xy index, cost, & parent index)
     openset[calc_index(nstart, xwidth, minx, miny)] = nstart
     print(calc_index(nstart, xwidth, minx, miny))
-
+    
     """
     loop
     """
@@ -217,92 +217,93 @@ def a_star_planning(sx, sy, gx, gy, rewardMatrix, greso, rs, xwidth, minx, miny,
         # find a calc_index whose total cost (cost + heuristic cost) is minimum from 'openset'
         # openset[o].cost = distance from current node to node in openset
         # calc_heuristc = heuristic cost
-
-        rewardWeight = 0
-
-        c_id = min(openset, key=lambda o: openset[o].cost + rewardWeight*costMatrix[int(o)] + 3*calc_heuristic(ngoal, openset[o]))
+        
+        rewardWeight = 1
+        
+        c_id = min(openset, key=lambda o: openset[o].cost + rewardWeight*costMatrix[int(o)] + 0*calc_heuristic(ngoal, openset[o]))
         current = openset[c_id]
+        current.cost += costMatrix[int(calc_index(current, xwidth, minx, miny))]
         print(current.cost)
-
+        
         # check the terminal condition
         if (current.x == ngoal.x) and (current.y == ngoal.y):
             print("Find goal")
-
+            
             ngoal.pind = current.pind
             ngoal.cost = current.cost
             break
-
+        
         # delete the key, c_id, from openset (dictionary)
         del openset[c_id]
         # add current with the key, c_id, to closedset (this is also a dictionary)
         closedset[c_id] = current
-
+        
         # consider the surrounding nodes of current node
         for i, _ in enumerate(motion):
               # these nodes' parent is current node
               node = Node(current.x + motion[i][0],
                           current.y + motion[i][1],
                           current.cost + motion[i][2], c_id)
-
+              
               # also calculate the calc_index about surrounding nodes
-              n_id = calc_index(node, xwidth, minx, miny)
-
+              n_id = calc_index(node, xwidth, minx, miny)  
+              
               # if n_id is already in the closedset, just continue
               if n_id in closedset:
-                  continue
-
-              # if the surrounding node is obstacle, this node will not be included to openset...
+                  continue 
+              
+              # if the surrounding node is obstacle, this node will not be included to openset... 
               if not verify_node(node, obmap, minx, miny, maxx, maxy):
-                  continue
-
-              # if n_id is not already in the closedset, add it to openset
+                  continue 
+              
+              # if n_id is not already in the closedset, add it to openset  
               if n_id not in openset:
                   openset[n_id] = node
-
+              
               else:
                   # replace the parent index if the following condition is satisfied
                   if openset[n_id].cost >= node.cost:
-                      openset[n_id] = node
-
+                      openset[n_id] = node   
+    
     """
     calculate the final path
     """
     rx, ry = calc_final_path(ngoal, closedset, greso)
-
+    
     """
     calculate the accumulated reward along the final path
     """
     rx = np.array(rx)
     ry = np.array(ry)
     gridx = rx / greso
-    gridy = ry / greso
-
+    gridy = ry / greso 
+    
     rewardPath = []
     for w in range(len(gridx)):
         rewardPath.append(rewardMatrix[int(gridx[w])][int(gridy[w])])
-
+        
     accumReward = np.sum(rewardPath)
-
-
+    
+    
     return rx, ry, costMatrix, accumReward
 
 
 def main():
     #sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
-
+    
     """
-    image2reward
+    image2reward 
     """
     # initialization of image and rewardMatrix
     # 1001 * 1001 pixels
-    image_file = '../img/atacamaTexture1001.png'
+    image_file = '1001atacamastarsall.png'
     img = cv2.imread(image_file)
     rewardMatrix = np.zeros((img.shape[0], img.shape[1]))
-
+    
     # extract specific color pixels (each mask has 966*1048 pixels)
     mask_star, mask_red, mask_lgray, mask_dgray, mask_spink, mask_white, mask_yellow, mask_green = color_detect(img)
 
-    # by using each mask, extract the specific color regions
+    # by using each mask, extract the specific color regions 
     index_star = np.vstack((np.where(mask_star == 255)[0], np.where(mask_star == 255)[1]))
     index_red = np.vstack((np.where(mask_red == 255)[0], np.where(mask_red == 255)[1]))
     index_lgray = np.vstack((np.where(mask_lgray == 255)[0], np.where(mask_lgray == 255)[1]))
@@ -335,9 +336,9 @@ def main():
         rewardMatrix[index_white[0][i]][index_white[1][i]] = 2
 
     for i in range(index_green.shape[1]):
-        rewardMatrix[index_green[0][i]][index_green[1][i]] = 5
+        rewardMatrix[index_green[0][i]][index_green[1][i]] = 5         
 
-    # from "pixel coordinate" to "xy coordinate"
+    # from "pixel coordinate" to "xy coordinate" 
     # below this line, we must use this 'rewardMatrix'
     rewardMatrix = rewardMatrix.T
     print(rewardMatrix.shape) # (1048, 966) --> (1001, 1001)
@@ -362,26 +363,26 @@ def main():
     maxx = 1001.0   # [m]
     miny = 0.0      # [m]
     maxy = 1001.0   # [m]
-
+    
     xwidth = round(maxx - minx)
     #ywidth = round(maxy - miny)
-
+    
     # execute a* algorithm which returns the optimal path
     rx, ry, costMatrix, accumReward = a_star_planning(sx, sy, gx, gy, rewardMatrix, greso, rs, xwidth, minx, miny, maxx, maxy)
 
     """
     plot
     """
-    xgrid_show, ygrid_show = np.mgrid[minx:maxx+greso:greso, miny:maxy+greso:greso]
-
-    fig = plt.figure(figsize=(20,20))
-
-    ax1 = fig.add_subplot(111)
+    xgrid_show, ygrid_show = np.mgrid[minx:maxx+greso:greso, miny:maxy+greso:greso] 
+    
+    fig = plt.figure(figsize=(20,20)) 
+    
+    ax1 = fig.add_subplot(121)
     ax1.set_xlim(minx - 1, maxx + 1); ax1.set_ylim(miny - 1, maxy + 1)
     ax1.set_aspect('equal')
     ax1.set_xlabel('x [m]'); ax1.set_ylabel('y [m]')
     ax1.set_title('Initial Heat Map with a* path plan')
-    im1 = ax1.pcolor(xgrid_show, ygrid_show, rewardMatrix, cmap='plasma', vmin=-5.0, vmax=10.0)
+    im1 = ax1.pcolor(xgrid_show, ygrid_show, rewardMatrix.T, cmap='plasma', vmin=-5.0, vmax=10.0)
     ax1_divider = make_axes_locatable(ax1)
     cax1 = ax1_divider.append_axes("right", size="7%", pad="2%")
     fig.colorbar(im1, cax=cax1)
@@ -389,15 +390,15 @@ def main():
     ax1.plot(gx, gy, c='darkgreen', marker='o')
     ax1.plot(rx, ry, "-k", linewidth=3)
     ax1.text(650, 950, "total reward: " + str(int(accumReward)), size = 15, weight="bold", color = "w" )
-
-    '''
+    
+    
     ax2 = fig.add_subplot(122)
     ax2.set_xlim(minx - 1, maxx + 1); ax2.set_ylim(miny - 1, maxy + 1)
     ax2.set_aspect('equal')
     ax2.set_xlabel('x [m]'); ax2.set_ylabel('y [m]')
     ax2.set_title('Initial Cost Map')
     dispcostMatrix = np.reshape(costMatrix, (rewardMatrix.shape[0], rewardMatrix.shape[1]))
-    im2 = ax2.pcolor(xgrid_show, ygrid_show, dispcostMatrix, cmap='viridis', vmin= 0, vmax=15.0)
+    im2 = ax2.pcolor(xgrid_show, ygrid_show, dispcostMatrix.T, cmap='viridis', vmin= 0, vmax=15.0)
     ax2_divider = make_axes_locatable(ax2)
     cax2 = ax2_divider.append_axes("right", size="7%", pad="2%")
     fig.colorbar(im2, cax=cax2)
@@ -405,10 +406,10 @@ def main():
     highreward_y = 100 # [m]
 #    ax2.plot(sx, sy, c='lime', marker='x')
 #    ax2.plot(gx, gy, c='gold', marker='o')
-    ax2.plot(rx, ry, "-k", linewidth=3)
+    ax2.plot(rx, ry, "-k", linewidth=3)    
     ax2.plot(highreward_x, highreward_y, c='orange', marker='*', markersize=15)
-    '''
+    
     plt.savefig('a*_path_plan_based_on_reward.jpg')
-
+    
 if __name__ == '__main__':
-    main()
+    main()    
